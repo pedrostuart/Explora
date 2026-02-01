709615cb5cebc161d1063936551d353eaf2a5174
pedrostuart <pedrohsmoura897@gmail.com>
2026-02-01 19:10:21 -0300

    .

---
+++ javascript/barra-pesquisa-mapa.js
@@ -0,0 +1,11 @@
+
+let btnPesquisar = document.querySelector(".barra_pesquisa .btn_pesquisar")
+
+btnPesquisar.addEventListener('click', ()=>{
+    let valorPesquisaLocal = document.querySelector(".input-pesquisa").value
+    let eventos = `${valorPesquisaLocal.trim()}`
+    let formatText = eventos.replaceAll(" ", "+")
+    let mapa = document.querySelector(".mapa iframe")
+
+    mapa.src = `https://www.google.com/maps?q=${formatText}&z=10&output=embed`
+})
