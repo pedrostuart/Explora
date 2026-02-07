7d787b73468ecfafc18f7895ff78aa55cf1bdb98
pedrostuart <pedrohsmoura897@gmail.com>
2026-02-07 18:34:09 -0300

    .

--- javascript/eventos-mapa.js
+++ javascript/eventos-mapa.js
@@ -1,8 +1,8 @@
 /*ABRIR MENU FILTROS*/
-let btnFiltrar = document.querySelector(".btn-preferencia-resposivo")
+let btnFiltrarResponsivo = document.querySelector(".btn-preferencia-resposivo")
 let menuFiltrar = document.querySelector(".menu-lateral")
 
-btnFiltrar.addEventListener("click", (e) =>{
+btnFiltrarResponsivo.addEventListener("click", (e) =>{
     e.preventDefault()
     menuFiltrar.style.display = 'flex'
 })
@@ -16,4 +16,54 @@ let fecharMenuFiltros = document.querySelector(".fechar-link .fechar-menu")
 fecharMenuFiltros.addEventListener("click", ()=>{
     
     menuFiltrar.style.display = ''
+})
+
+
+let btnFiltrar = document.querySelector(".btn-filtrar .btn-preferencia")
+/*Datas*/
+let labelsDatas = document.querySelectorAll(".label-preferencias input")
+let valorData
+labelsDatas.forEach(btns =>{
+    btns.addEventListener("click", ()=>{
+        valorData = btns.value
+    })
+})
+/*Preferencias*/
+
+let labelsGostos= document.querySelectorAll(".label-preferencias button")
+
+let valorPreferencia = []
+labelsGostos.forEach(btns =>{
+    btns.addEventListener("click", ()=>{
+        
+        if(btns.className == ""){
+            btns.classList.add("selecionado")
+            valorPreferencia.push(btns.value)
+        }else{
+            
+            btns.classList.remove("selecionado")
+            let posicao = valorPreferencia.indexOf(btns.value)/*indexOf olha o que ta dentro do array e compara com valor do btns*/ 
+            valorPreferencia.splice(posicao, 1)
+        }
+    })
+})
+
+/*valores radios*/
+
+let radiosKm = document.querySelectorAll(".radio")
+let valorRadio     
+    radiosKm.forEach(radios =>{
+    radios.addEventListener("click", ()=>{
+        valorRadio = radios.value
+    })
+})
+
+/*valor input*/
+
+btnFiltrar.addEventListener("click", ()=>{
+    console.log(valorData)
+    console.log(valorRadio)
+    let orcamento = Number(document.getElementById("preco").value)
+    console.log(orcamento)
+    console.log(valorPreferencia)
 })
