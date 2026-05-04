26722a0e3475c3d3da69de8149f64273d5745eca
pedrostuart <pedrohsmoura897@gmail.com>
2026-05-04 19:29:53 -0300

    .

--- javascript/barra-pesquisa-mapa.js
+++ javascript/barra-pesquisa-mapa.js
@@ -9,5 +9,26 @@ btnPesquisar.addEventListener('click', ()=>{
     let mapa = document.querySelector(".mapa iframe")
     mapa.src = `https://www.google.com/maps?q=${formatText}&z=13&output=embed`
 })
+let btnFiltrarMapa = document.querySelector(".btn-filtrar .btn-preferencia-mapa")
+let labelsDatas = document.querySelectorAll(".filtrar-distancia radios");
+let valorData;
+let ultimoRadioDataClicado = null;
+
+labelsDatas.forEach(radio => {
+    radio.addEventListener("click", () => {
+        // Esta lógica permite desmarcar o botão de rádio clicando nele novamente
+        if (ultimoRadioDataClicado === radio) {
+            radio.checked = false;
+            valorData = undefined;
+            ultimoRadioDataClicado = null;
+        } else {
+            valorData = radio.value;
+            ultimoRadioDataClicado = radio;
+        }
+    })
+})
+btnFiltrarMapa.addEventListener("click", ()=>{
+    console.log(valorData)
+})
 
 
