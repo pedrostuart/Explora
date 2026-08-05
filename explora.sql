adf62db398d3292a65519feec4daf395d6bde2b8
pedrostuart <pedrohsmoura897@gmail.com>
2026-08-05 20:28:08 -0300

    .

--- explora.sql
+++ explora.sql
@@ -7,6 +7,26 @@ create table `usuarios`(
     `nome` varchar(240) not null,
     `email` varchar(140) not null,
     `telefone` varchar(12) not null,
-    `senha` VARCHAR(255) not null
+    `senha` VARCHAR(255) not null,
+    `capacidade` int(8) not null,
+    `descricao` varchar(1000) not null
+    --local
+    --data de nascimento
+    --orçamento disponivel
     PRIMARY KEY("id")
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+create table `eventos`(
+    `id` int unsigned not null auto_increment,
+    `nome_evento` varchar(240) not null,
+    `data` date not null,
+    `hora` time not null,
+    `local` varchar(240) not null,
+    `link_compra` VARCHAR(255) not null,
+    `preco` varchar(20) not null,
+    PRIMARY KEY(`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--eventos visitados
+--preferencias
+--evento favorito
