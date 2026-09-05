64680a601d6ad6d29396d824ad9e616dab0a7559
pedrostuart <pedrohsmoura897@gmail.com>
2026-09-05 18:29:09 -0300

    .

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { EventosModule } from './eventos/eventos.module';
 
 
 @Module({
@@ -7,7 +8,8 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       //para que as variaveis presententes no .env consiga ser visto por toda aplicação
       isGlobal: true
-    })
+    }),
+    EventosModule
   ],
   controllers: [],
   providers: []
